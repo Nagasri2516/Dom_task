a212ce189f29ae34a7d5d9543fff3a99471a903d
Nagasri2516 <nagasrikarumuri2516@gmail.com>
2026-03-15 17:40:50 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+function img1(){
+document.getElementById("slide").src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQbheqVGpreSdaEEY12XgLQrxgqq6jgj6SrrQ&s";
+}setInterval(img1,6000);
+
+function img2(){
+document.getElementById("slide").src="https://simprosys.com/simprotips/wp-content/uploads/2025/06/Getting-Started-with-Microsoft-Advertising.jpg";
+}setInterval(img2,2000);
